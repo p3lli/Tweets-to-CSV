0aef371f5e63edd06f788a6ce4276eeb1142c1d6
Alessio <alessio@email.it>
2020-04-02 16:42:43 +0200

    Improved params validations

--- utils/utils.py
+++ utils/utils.py
@@ -8,15 +8,15 @@ def validate_args(args):
     - 'out_dir' must be an existing directory
     """
     valid_args = True
-    if len(args.query_word) > const.TWEET_MAX_LEN and args.search_type == 'by-user':
+    if args.query_word and len(args.query_word) > const.TWEET_MAX_LEN and args.search_type == 'by-user':
         valid_args = False
         logging.error('ERROR: \'{}\' is not a valid user name'
                       .format(args.query_word))
-    if os.path.isdir(args.out_dir) == False:
+    if args.out_dir and os.path.isdir(args.out_dir) == False:
         valid_args = False
         logging.error('ERROR: \'{}\' is not an existing directory'
                 .format(args.out_dir))
-    if not args.ntweets.isdigit() or args.ntweets < 0:
+    if args.ntweets and (not args.ntweets.isdigit() or args.ntweets < 0):
         valid_args = False
         logging.error('ERROR: \'{}\' is not a valid value for --number-of-tweets'
                 .format(args.ntweets))
